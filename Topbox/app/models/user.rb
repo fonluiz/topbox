c5e2896eeec1837c6095810a5586e6d0eb287ec8
gabrielmla <gabmla19@gmail.com>
2016-07-24 20:37:22 -0300

    User model now validates user first_name, last_name, username, email and password fields.

--- Topbox/app/models/user.rb
+++ Topbox/app/models/user.rb
@@ -2,4 +2,11 @@ class User < ApplicationRecord
 
   has_secure_password
 
+  validates :first_name, :last_name, presence: true
+  validates :username, length: { minimum: 6,
+    too_short: "Username needs to have at least %{count} characters." }
+  validates :email, uniqueness: true
+  validates :password, length: { in: 6..20,
+    wrong_length: "Password size must be between %{count}." }
+
 end
