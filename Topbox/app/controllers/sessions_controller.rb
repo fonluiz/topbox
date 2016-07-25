1ebe524a6d595d3e03b803bc587c02831a8a4b8c
gabrielmla <gabmla19@gmail.com>
2016-07-24 22:02:11 -0300

    'Front-end validation of field on login and signup screen's - Part 1'

--- Topbox/app/controllers/sessions_controller.rb
+++ Topbox/app/controllers/sessions_controller.rb
@@ -4,11 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user_not_found = false
+    @user_wrong_password = false
+
     @user = User.find_by_email(params[:session][:email])
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
       redirect_to '/'
     else
+      @user_not_found = true unless @user
       redirect_to 'login'
     end
   end
