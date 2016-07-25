a31f64d0a3903dccb4339c774c7771cbbf2dad9e
gabrielmla <gabmla19@gmail.com>
2016-07-24 22:09:45 -0300

    Correction on user model validation

--- Topbox/app/models/user.rb
+++ Topbox/app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   validates :first_name, :last_name, presence: true
   validates :username, length: { minimum: 6,
     too_short: "Username needs to have at least %{count} characters." }
-  validates :email, uniqueness: true
+  validates :email, presence: true, uniqueness: true
   validates :password, length: { in: 6..20,
     wrong_length: "Password size must be between %{count}." }
 
