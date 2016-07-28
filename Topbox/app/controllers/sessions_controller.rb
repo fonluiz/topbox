0da60e1e9075e9b45c24bb69e2ea9f81a402c847
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2016-07-27 22:19:47 -0300

    Authenticate using username
    Now the users can also sign in using their username.

--- Topbox/app/controllers/sessions_controller.rb
+++ Topbox/app/controllers/sessions_controller.rb
@@ -4,12 +4,15 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_email(params[:session][:email])
-    if @user && @user.authenticate(params[:session][:password])
-      session[:user_id] = @user.id
+    @user_email = User.find_by_email(params[:session][:email])
+    @username = User.find_by(params[:session][:username])
+    if @user_email && @user_email.authenticate(params[:session][:password]) ||
+        @username && @username.authenticate(params[:session][:password])
+      session[:user_id] ||= @user_email.id
+      session[:user_id] ||= @username.id
       redirect_to '/home'
     else
-      flash.now[:error] = "Invalid username/password combination."
+      flash.now[:error] = 'Invalid username/password combination.'
       render :new
     end
   end
