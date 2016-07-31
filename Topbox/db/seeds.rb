ce614662ce6b5b6e7d6d21baeeee417982f86b12
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2016-07-31 13:37:38 -0300

    Create seeds for the database

--- Topbox/db/seeds.rb
+++ Topbox/db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Luiz', last_name: 'Fonseca', username: 'luizfo',
+            email: 'luiz@gmail.com', password: 'luiz123')
+
+User.create(first_name: 'Ítalo', last_name: 'Batista', username: 'italob',
+            email: 'italo@gmail.com', password: 'italo123')
+
+User.create(first_name: 'Jair', last_name: 'Neto', username: 'jairne',
+            email: 'jair@gmail.com', password: 'jair123')
+
+User.create(first_name: 'Laybson', last_name: 'Cunha', username: 'laybsonc',
+            email: 'laybson@gmail.com', password: 'laybson123')
+
+User.create(first_name: 'Gabriel', last_name: 'Morais', username: 'gabrielm',
+            email: 'gabriel@gmail.com', password: 'gabriel123')
