b2f446cb8295672556e1502fdf4f417d96ff6d7e
gabrielmla <gabmla19@gmail.com>
2016-07-31 05:38:17 -0300

    Sign up screen don't auto-login, after success on creating a new user, anymore.

--- Topbox/app/controllers/users_controller.rb
+++ Topbox/app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
       flash[:success] = 'Conta criada com sucesso!'
       redirect_to '/login'
     else
