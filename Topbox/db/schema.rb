9be5212a02c418bba6bb7fd17714fbaf9b9ce788
Italo Batista <italo.batista@ccc.ufcg.edu.br>
2016-07-31 21:59:26 -0300

    Put database's table at conventions
    Rename database's columns

--- Topbox/db/schema.rb
+++ Topbox/db/schema.rb
@@ -12,14 +12,14 @@
 
 ActiveRecord::Schema.define(version: 20160731171849) do
 
-  create_table "homes", force: :cascade do |t|
+  create_table "home", force: :cascade do |t|
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_homes_on_user_id"
   end
 
-  create_table "users", force: :cascade do |t|
+  create_table "user", force: :cascade do |t|
     t.string   "first_name"
     t.string   "last_name"
     t.string   "username"
