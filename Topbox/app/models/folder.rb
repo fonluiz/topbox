56c146d64c6a68eb8dfd718cc80d5c55e4c710ad
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2016-08-08 19:13:35 -0300

    Adjust association between documents and folders

--- Topbox/app/models/folder.rb
+++ Topbox/app/models/folder.rb
@@ -2,5 +2,6 @@ class Folder < ApplicationRecord
   belongs_to :user
   belongs_to :parent, class_name: 'Folder'
   has_many :children, :dependent => :destroy, class_name: 'Folder', :foreign_key => 'parent_id'
+  has_many :documents, :dependent => :destroy, class_name: 'Document'
   validates :name, presence: true
 end
