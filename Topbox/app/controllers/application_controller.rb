35458a5f91fb665b3c19579ff2849e931837660c
fonluiz <luiz.fonseca@ccc.ufcg.edu.br>
2016-08-08 19:22:13 -0300

    Revert "Adding the gems files for metrics analysis and commenting the methods"
    This reverts commit b6138e5204da6bd5a3dfabea99e28d44c3b71753.

--- Topbox/app/controllers/application_controller.rb
+++ Topbox/app/controllers/application_controller.rb
@@ -8,27 +8,23 @@ class ApplicationController < ActionController::Base
   before_filter :show_navbar
 
 
-  #Set the current user if there is a session.
+
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
 
-  #Finds the current folder using the current user id and return it.
   def current_folder
     return (@@current_folder ||= Folder.find_by(name: 'My TopBox', user_id: current_user.id))
   end
 
-  #Receive a folder as argument and set the current folder to it.
   def set_current_folder(folder)
     @@current_folder = folder
   end
 
-  #If there isn't a current user no matter what page is acessed the user is redirected to the login page.
   def require_user
     redirect_to '/login' unless current_user
   end
 
-  #
   def save_login_state
     if session[:user_id]
       redirect_to(:controller => 'folders', :action => 'new')
@@ -38,22 +34,18 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  #Log the user out.
   def log_out
     session[:user_id] = nil
   end
 
-  #Receive an user as agurment and log him in.
   def log_in(user)
     session[:user_id] = user.id
   end
 
-  #See if has an user logged in.
   def logged_in?
     !current_user.nil?
   end
 
-  #Show the nav bar.
   protected
   def show_navbar
     @show_navbar = true
