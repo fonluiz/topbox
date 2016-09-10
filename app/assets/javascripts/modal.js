f76f9e74db976c99166c4558fbab31f67c8dd196
Rafaela <rafaela9136@gmail.com>
2016-09-10 14:54:59 -0300

    fix folder creation on firefox

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,6 +1,8 @@
+var idModal;
+
 $(document).ready(function() {
 	$(".opm").on('click', function() {
-		var idModal = $(this).data('modal');
+		idModal = $(this).data('modal');
 		$(idModal).modal("show");
 	})
 });
