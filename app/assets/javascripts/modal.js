78f0dd7600fd426903daf8631224ec88bc011a33
Rafaela <rafaela9136@gmail.com>
2016-09-10 13:59:08 -0300

    Solving bug when creating doc/folder

--- app/assets/javascripts/modal.js
+++ app/assets/javascripts/modal.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
 	$(".opm").on('click', function() {
 		var idModal = $(this).data('modal');
-		console.log(idModal);
 		$(idModal).modal("show");
 	})
 });
+
+function refresh() {
+	if(location.search.indexOf('reloaded=yes') < 0){
+		var hash = window.location.hash;
+		var loc = window.location.href.replace(hash, '');
+		loc += (loc.indexOf('?') < 0? '?' : '&') + 'reloaded=yes';
+		setTimeout(function(){window.location.href = loc + hash;}, 0);
+	}
+}
