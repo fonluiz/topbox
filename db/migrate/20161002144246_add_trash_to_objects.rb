05c664b2e8ccc5cca86e9fbf0275be40be66f03d
oandrevictor <oandrevictor@gmail.com>
2016-10-02 11:58:45 -0300

    Reset migrations

--- db/migrate/20161002144246_add_trash_to_objects.rb
+++
@@ -1,6 +0,0 @@
-class AddTrashToObjects < ActiveRecord::Migration[5.0]
-  def change
-  	add_column :folders, :trash, :boolean, default: false
-  	add_column :documents, :trash, :boolean, default: false
-  end
-end
