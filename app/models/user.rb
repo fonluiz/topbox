fe21835830fdf309125825a0fac1c33ea62ff1d0
Rafaela <rafaela9136@gmail.com>
2016-10-02 22:01:28 -0300

    addign button to edit language

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates :username, uniqueness: true, length: { in: 6..30 }
   validates :email, presence: true, uniqueness: true
   validates :password, confirmation: true, length: { in: 6..30 }, :if => :password
-  validates :password_confirmation, presence: true
+  validates :password_confirmation, presence: true, :if => :password
 
   def get_name
     self.first_name + ' ' + self.last_name
